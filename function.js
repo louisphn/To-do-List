d7cac4f283d2501612bee11a70660f7f7a3da142
Louis Phan <72560298+louisphn@users.noreply.github.com>
2021-04-07 11:50:58 +0900

    Add files via upload

--- function.js
+++ function.js
@@ -1,44 +1,51 @@
-$(".new").click(function(){
-   var str= $(".content").val();
-  $("ul").append("<li class='item'><label class='item__checkbox'><input type='checkbox'><i class='fas fa-check'></i></label>" + str + "<button class='item__delete'><i class='fas fa-trash-alt'></i></button></li>");
-  $(".content").val('');
+function deleteItem() {
+  $(document).on("click", ".item__delete", function () {
+    $(this).closest("li").remove();
+  });
+}
+
+function complete() {
+  $(document).on("click", "input[type=checkbox]", function () {
+    $(this).closest("li").toggleClass("done");
+  });
+}
+
+$(".new").click(function () {
+  var str = $(".content").val();
+  $("ul").append(
+    "<li class='item'><label class='item__checkbox'><input type='checkbox'><i class='fas fa-check'></i></label>" +
+      str +
+      "<button class='item__delete'><i class='fas fa-trash-alt'></i></button></li>"
+  );
+
   var priority = $("input:checked").val();
-  console.log("item__checkbox--" + priority);
-if (priority !== 0) {
-    var a= $("ul li:last-child").find("label");
+
+  if (priority !== 0) {
+    var a = $("ul li:last-child").find("label");
     $(a).addClass("item__checkbox--" + priority);
   }
-  $(".item__delete").click(function () {
-    $(this).closest("li").remove();
-   })
-  $("input[type=checkbox]").click(function(){
-    $(this).closest("li").toggleClass("done");
-  })
-})
 
-  $(".item__delete").click(function() {
-  $(this).closest("li").remove();
-})
+  $(".content").val("");
+});
 
- $("input[type=checkbox]").click(function(){
-    $(this).closest("li").toggleClass("done");
-  })
+complete();
+deleteItem();
 
-$(".nav__item").click(function(){
+$(".nav__item").click(function () {
   $(".nav__item").removeClass("active");
   $(this).addClass("active");
-})
+});
 
-$(".all").click(function(){
+$(".all").click(function () {
   $(".item").show();
-})
+});
 
-$(".now").click(function(){
+$(".now").click(function () {
   $(".item").show();
   $(".done").hide();
-})
+});
 
-$(".complete").click(function(){
+$(".complete").click(function () {
   $(".item").show();
   $(".item:not(.done)").hide();
-})
+});
